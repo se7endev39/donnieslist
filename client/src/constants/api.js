6b7b373e4784caae1dfe47f017016897895ef78f
varunbhandia <varunbhandia@gmail.com>
2021-07-31 23:22:46 +0530

    Fix | University Field Update

--- client/src/constants/api.js
+++ client/src/constants/api.js
@@ -3,12 +3,12 @@ export const tokBoxApikey = '45801242';
 
 /* server */
 
-//  export const API_URL = 'https://www.donnieslist.com/api';
-//  export const Image_URL = 'https://www.donnieslist.com';
-//  export const CLIENT_ROOT_URL = 'https://www.donnieslist.com';
+ export const API_URL = 'https://www.donnieslist.com/api';
+ export const Image_URL = 'https://www.donnieslist.com';
+ export const CLIENT_ROOT_URL = 'https://www.donnieslist.com';
 
 /* localhost */
 
-export const CLIENT_ROOT_URL = 'http://127.0.0.1:5000';
-export const API_URL = 'http://127.0.0.1:3000';
-export const Image_URL = 'http://127.0.0.1:5000';
+// export const CLIENT_ROOT_URL = 'http://127.0.0.1:5000';
+// export const API_URL = 'http://127.0.0.1:3000';
+// export const Image_URL = 'http://127.0.0.1:5000';
