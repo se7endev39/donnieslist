12f64d7a3b17ad5984f0f833a2059b1e6d9d0e7b
horix7 <horix77777@gmail.com>
2021-08-22 02:40:15 +0200

    modified build

--- client/src/constants/api.js
+++ client/src/constants/api.js
@@ -3,7 +3,7 @@ export const tokBoxApikey = '45801242';
 
 
 export const CLIENT_ROOT_URL = 'https://donnieslist.com';
-export const API_URL = 'http://api.donnieslist.com';
+export const API_URL = 'https://api.donnieslist.com';
 export const Image_URL = 'https://images.donnieslist.com/';
 
 // export const CLIENT_ROOT_URL = "http://localhost:3001";
