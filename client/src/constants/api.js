54a9f3e46be8495801c5ad158d53acfbb7198de4
dreamwell <dreamwell506@outlook.com>
2021-08-25 13:56:47 -0700

    fix: upload directory path

--- client/src/constants/api.js
+++ client/src/constants/api.js
@@ -3,11 +3,11 @@ export const tokBoxApikey = '45801242';
 
 
 export const CLIENT_ROOT_URL = 'https://donnieslist.com';
-export const API_URL = 'http://donnieslist.com:3012';
+export const API_URL = 'https://api-erik.donnieslist.com';
 export const Image_URL = 'https://images.donnieslist.com/';
 
 // export const CLIENT_ROOT_URL = "http://localhost:3001";
-// export const API_URL = "http://localhost:4000";
+// export const API_URL = "http://137.184.18.110:3012";
 // export const Image_URL = "http://127.0.0.1:4000/uploads/";
 
 export const DEVELOPMENT_URL = ''
